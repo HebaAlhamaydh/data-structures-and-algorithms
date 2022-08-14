c9ba731ffa97bfb60fbe344807ac9860ecb9fa04
HebaAlhamaydh <Eng_heba_hamaydh@hotmail.com>
2022-08-14 21:56:16 +0300

    quicksort

---
+++ javascript/quickSort/__test__/quick.test.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const quickSort  = require('../quicksort');
+describe(' quickSort Test', () => {
+
+    test(' quickSort an array', () => {
+        let arr = [8, 4, 23, 42, 16, 15];
+        quickSort(arr, 0, arr.length - 1);
+        expect(arr).toEqual([4, 8, 15, 16, 23, 42]);
+      });
+      it('Reverse-sorted', () => {
+        const arr = [20, 18, 12, 8, 5, -2];
+        quickSort(arr, 0, arr.length - 1);
+        expect(arr).toEqual([-2, 5, 8, 12, 18, 20]);
+      });
+      it('Few uniques', () => {
+        const arr = [5, 12, 7, 5, 5, 7];
+        quickSort(arr, 0, arr.length - 1);
+        expect(arr).toEqual([5, 5, 5, 7, 7, 12]);
+      });
+      it('Nearly-sorted', () => {
+        const arr = [2, 3, 5, 7, 13, 11];
+        quickSort(arr, 0, arr.length - 1);
+        expect(arr).toEqual([2, 3, 5, 7, 11, 13]);
+      });
+
+
+})
